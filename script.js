daba7422b3074d16f0b31ce3bd69dca714c3cc12
Pari <62230627+kamath-parnika@users.noreply.github.com>
2022-08-08 02:22:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+https://coolors.co/palette/11270b-71b340-669d31-598b2c-3c5a14
+https://coolors.co/palette/143601-1a4301-245501-538d22-73a942-aad576
+https://coolors.co/palette/a1cca5-8fb996-709775-415d43-111d13
+
+
+*/
