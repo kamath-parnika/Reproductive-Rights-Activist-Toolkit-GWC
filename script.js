28f6a13b84d1089452dacb3a263ca1dc156eb784
Pari <62230627+kamath-parnika@users.noreply.github.com>
2022-08-08 02:25:50 +0000

    pre austin setup

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 https://coolors.co/palette/11270b-71b340-669d31-598b2c-3c5a14
 https://coolors.co/palette/143601-1a4301-245501-538d22-73a942-aad576
 https://coolors.co/palette/a1cca5-8fb996-709775-415d43-111d13
-
-
+https://developers.google.com/speed/libraries/#web-font-loader
+https://stop-asian-hate.cherilynnchow1.repl.co/
+https://colorofchange.org/
 */
